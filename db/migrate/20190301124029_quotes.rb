15187cd8a55f9725f329b84bece8ce36e7376bac
George <gga1007001@gmail.com>
2019-03-01 16:19:44 +0300

    add ticket

--- db/migrate/20190301124029_quotes.rb
+++ db/migrate/20190301124029_quotes.rb
@@ -1,11 +1,15 @@
 class Quotes < ActiveRecord::Migration[5.2]
   def change
+    create_table :quotes do |t|
       t.float :open
       t.float :high
       t.float :low
       t.float :close
+      
       t.date  :close_time
-
+      t.string :ticket
+      
       t.timestamps
+    end
   end
 end
