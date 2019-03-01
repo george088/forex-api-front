3ec60bcb5ade1198f62fbfbb993012939739eb1e
George <gga1007001@gmail.com>
2019-03-01 16:44:37 +0300

    new migrations for table Quotes

--- db/migrate/20190301134252_create_quotes.rb
+++ db/migrate/20190301134252_create_quotes.rb
@@ -1,6 +1,13 @@
 class CreateQuotes < ActiveRecord::Migration[5.2]
   def change
     create_table :quotes do |t|
+      t.float :open
+      t.float :high
+      t.float :low
+      t.float :close
+      
+      t.date  :close_time
+      t.string :ticket
 
       t.timestamps
     end
