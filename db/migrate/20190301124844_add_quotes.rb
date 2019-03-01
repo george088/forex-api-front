c2d0719b063475be5d23961434484e2a7d0c508c
George <gga1007001@gmail.com>
2019-03-01 16:32:33 +0300

    fix download path

--- db/migrate/20190301124844_add_quotes.rb
+++ db/migrate/20190301124844_add_quotes.rb
@@ -6,7 +6,7 @@ class AddQuotes < ActiveRecord::Migration[5.2]
     Ticketlist.select(:ticket).each do |element|
 
       url = "https://candledata.fxcorporate.com/D1/#{element.ticket}/2012.csv.gz"
-      folder = "#{Rails.root}/downloaded/"
+      folder = "#{Rails.root}/public/downloaded/"
       file_name = "#{element.ticket}_2012"
       
       full_path = folder + file_name + '.csv.gz'
@@ -18,8 +18,9 @@ class AddQuotes < ActiveRecord::Migration[5.2]
       Zlib::GzipReader.open(full_path).each_line do |gz|
           puts line = gz.split(',')
           if !(line[0] == 'DateTime')
-            Quote.create(close_time: Date.strptime(line[0].split(' ')[0], '%m/%d/%Y').to_s, open: line[1], high: line[2], low: line[3], close: line[4], ticket: element.ticket)
+            Quotes.create(close_time: Date.strptime(line[0].split(' ')[0], '%m/%d/%Y').to_s, open: line[1], high: line[2], low: line[3], close: line[4], ticket: element.ticket)
           end
       end
+    end
   end
 end
