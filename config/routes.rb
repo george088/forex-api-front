f651db050687795a1502854d3be017fd47705773
George <gga1007001@gmail.com>
2019-03-04 18:22:28 +0300

    get apikey by emai

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, :defaults => {:format => :json} do
     namespace :v1 do
-      resources :users, only: [:show, :create, :update, :destroy] 
+      resources :users, only: [:show, :create, :update, :destroy]
+      get 'api_key_by_email' => 'users#api_key_by_email' 
     end
   end
 end
