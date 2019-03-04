9b5bd4a64caeaf1a4bff01d779162954c3c27826
George <gga1007001@gmail.com>
2019-03-04 23:49:40 +0300

    del white-spaces

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,5 @@
 class Quote < ApplicationRecord
   def self.quotes tickets, type, from, to
-      Quote.where(ticket: tickets, close_time: from..to).pluck(type)
+    Quote.where(ticket: tickets, close_time: from..to).pluck(type)
   end
 end
