e0db53c49917e759a0f318b6bc435152d0d30eaa
George <gga1007001@gmail.com>
2019-03-04 13:59:52 +0300

    fix conflict

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
   # controller for access by api
   # GET /users/1
   # GET /users/1.json
@@ -16,32 +14,6 @@ class UsersController < ApplicationController
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
-=======
-=======
->>>>>>> Stashed changes
-  # GET /users
-  # GET /users.json
-  def index
-    @users = User.all
-  end
-
-  # GET /users/1
-  # GET /users/1.json
-  def show
-  end
-
-  # GET /users/new
-  def new
-    @user = User.new
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-  end
-
-  # GET /users/1/edit
-  def edit
-  end
 
   # POST /users
   # POST /users.json
@@ -50,23 +22,10 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
         # format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
         # format.html { render :new }
-=======
-=======
->>>>>>> Stashed changes
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
@@ -77,23 +36,10 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
         # format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
-        # format.html { render :edit }
-=======
-=======
->>>>>>> Stashed changes
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
         format.html { render :edit }
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
@@ -104,34 +50,16 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
     respond_to do |format|
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
       # format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-=======
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
->>>>>>> Stashed changes
-=======
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
->>>>>>> Stashed changes
       format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
     # get user by apikey
     def set_user
       @user = User.find_by(apikey: params[:key])
-=======
-    def set_user
-      @user = User.find(params[:id])
->>>>>>> Stashed changes
-=======
-    def set_user
-      @user = User.find(params[:id])
->>>>>>> Stashed changes
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
