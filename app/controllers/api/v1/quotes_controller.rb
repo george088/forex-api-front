b983d931183a42baf35331733ea63401a99b190f
George <gga1007001@gmail.com>
2019-03-04 18:55:12 +0300

    add Api::V1::   ... make check before action

--- app/controllers/api/v1/quotes_controller.rb
+++ app/controllers/api/v1/quotes_controller.rb
@@ -1,4 +1,5 @@
-class QuotesController < ApplicationController
+class Api::V1::QuotesController < ApplicationController
+  before_action :check_apikey
   before_action :set_quote, only: [:show, :edit, :update, :destroy]
 
   # GET /quotes
@@ -21,5 +22,9 @@ class QuotesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def quote_params
       params.fetch(:quote, {})
+    end
+
+    def check_apikey
+      render json: { message: 'Check key or create user', status: :unauthorized } if User.find_by(apikey: params[:key]).nil?
     end
 end
