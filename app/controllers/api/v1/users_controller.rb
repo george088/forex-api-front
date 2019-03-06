5d0f7f46cb470ee5818c2c46ad10a05149628f18
George <gga1007001@gmail.com>
2019-03-07 00:54:06 +0300

    show error with rescure_form

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,10 @@ class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :validate_params, only: [:api_key_by_email]
 
+  rescue_from RailsParam::Param::InvalidParameterError do |exeption|
+    render json: { error: exeption, status: :unprocessable_entity}
+  end
+
   # controller for access by api
   # GET /users/1
   # GET /users/1.json
